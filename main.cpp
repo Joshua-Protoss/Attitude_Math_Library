40a0b5d22b5d40256a5f1aced00782dd6b231e86
Muhammad Joshua <72489749+Joshua-Protoss@users.noreply.github.com>
2022-12-05 00:42:17 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+// System Includes
+#include <string>
+#include <iostream>
+#include <cmath>
+
+// Program Includes
+#include "AML.h"
+
+using namespace AML;
+
+int main(int argc, char **argv)
+{
+	//Vector3 v1(1.0, 2.0, 3.0);
+	//Vector3 v2(4.0, 5.0, 6.0);
+	
+	//Vector3 v3 = v1 + v2;
+	//std::cout << v1 << std::endl;
+	//std::cout << v2 << std::endl;
+	//std::cout << v3 << std::endl;
+	
+	double data[3][3] = {{1.2, 3.2, 0.5},{0.1, 1.0, 6.0},{-4.1, 6.0, 8.0}};
+	Matrix33 Mat1 = Matrix33(data);
+	Vector3 v1 = Vector3(1,0,0);
+	Vector3 v2 = Mat1 * v1;
+	
+	std::cout << v1 << std::endl;
+	std::cout << Mat1 << std::endl;
+	std::cout << v2 << std::endl;
+	return 0;
+}
